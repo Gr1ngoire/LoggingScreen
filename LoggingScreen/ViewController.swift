6b2d3d68bcf8636545010a476ff09e88076f1b8c
Gr1ngoire <gringoirelamusicart@gmail.com>
2021-04-11 00:02:05 +0300

    Added control of right name and password

--- LoggingScreen/ViewController.swift
+++ LoggingScreen/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var forgottenNameButton: UIButton!
+    @IBOutlet weak var forgottenPasswordButton: UIButton!
+    
+    @IBOutlet weak var alarmMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        alarmMessageLabel.isHidden = true
     }
+    
+    let passwordData: [String : String] = [
+        "Mykola" : "88005553535",
+        "Voland" : "666666666",
+        "Sherlock" : "BakerStreet221B"
+    ]
 
-
+    @IBAction func logInButtonAction(_ sender: UIButton) {
+        
+        alarmMessageLabel.isHidden = true
+        
+        guard nameInput.text != "", passwordInput.text != "" else {
+            alarmMessageLabel.text = "You need to enter name AND password"
+            alarmMessageLabel.isHidden = false
+            return
+        }
+        
+        if let passwordValue = passwordData[nameInput.text!], passwordValue == passwordInput.text {
+            print("Passed")
+        } else {
+            alarmMessageLabel.text = "There is no such name or password"
+            alarmMessageLabel.isHidden = false
+        }
+        
+    }
 }
 
